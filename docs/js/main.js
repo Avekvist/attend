cf9b144844cfd587523a1436f55c4c1eb0ba8349
alfonsohlsson <45263910+alfonsohlsson@users.noreply.github.com>
2019-05-14 15:04:50 +0200

    tags, menu and other

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,62 +4,63 @@ $(document).ready(function() {
   $('.nav-button').css('margin-left', 0);
   $('.nav-button').css('margin-top', 0);
   $('.nav-button').css('width', '96px');
+  $('.nav-button').css('display', 'none');
 
 
-  var number = 0;
+  var menuNotActive=0;
 
   $('nav #container img').css('cursor', 'pointer');
   $('header').css('margin-top', '0px');
 
   $('#logo').click(function() {
-    if (number == 0) {
+    if (menuNotActive==0) {
+
+      $('.nav-button').css('display', 'block');
+
       if ($(window).width() < 730) {
         $('header').animate({
           marginTop: '475px'
         });
         $('#ide').animate({
-          marginTop: '100px'
-        }, 400);
+          marginTop: '150px'
+        }, 300);
         $('#ide').animate({
-            paddingRight:'36%',
-            paddingLeft:'30%',
-            marginLeft:'-30%'
+          paddingRight: '36%',
+          paddingLeft: '30%',
+          marginLeft: '-30%'
 
-        }, 400);
-      /*  $('#ide').animate({
-          width:'75%'
-        }, 300);*/
+        }, 300);
 
         $('#order').animate({
-          marginTop: '225px'
-        }, 400);
+          marginTop: '275px'
+        }, 300);
         $('#order').animate({
-          paddingRight:'36%',
-          paddingLeft:'30%',
-          marginLeft:'-30%'
-        }, 400);
+          paddingRight: '36%',
+          paddingLeft: '30%',
+          marginLeft: '-30%'
+        }, 300);
 
 
         $('#about').animate({
-          marginTop: '350px'
-        }, 400);
+          marginTop: '400px'
+        }, 300);
         $('#about').animate({
-          paddingRight:'36%',
-          paddingLeft:'30%',
-          marginLeft:'-30%'
-        }, 400);
+          paddingRight: '36%',
+          paddingLeft: '30%',
+          marginLeft: '-30%'
+        }, 300);
 
 
         $('#contact').animate({
-          marginTop: '475px'
-        }, 400);
+          marginTop: '525px'
+        }, 300);
         $('#contact').animate({
-          paddingRight:'36%',
-          paddingLeft:'30%',
-          marginLeft:'-30%'
-        }, 400, function() {
+          paddingRight: '36%',
+          paddingLeft: '30%',
+          marginLeft: '-30%'
+        }, 300, function() {
 
-          $('.nav-button').css('z-index',0);
+          $('.nav-button').css('z-index', 0);
         });
 
 
@@ -67,35 +68,36 @@ $(document).ready(function() {
       } else {
         $('#ide').animate({
           marginLeft: '-300px'
-        }, 400);
+        }, 300);
 
         $('#order').animate({
           marginLeft: '-150px'
-        }, 400);
+        }, 300);
 
         $('#about').animate({
           marginLeft: '150px'
-        }, 400);
+        }, 300);
 
         $('#contact').animate({
           marginLeft: '300px'
-        }, 400, function() {
-          $('.nav-button').css('z-index',0);
+        }, 300, function() {
+          $('.nav-button').css('z-index', 0);
         });
       }
 
-      number = 1;
+      menuNotActive = 1;
+      console.log(menuNotActive);
     } else {
 
 
-      $('.nav-button').css('z-index',-1);
+      $('.nav-button').css('z-index', -1);
       $('#ide').animate({
         width: '96px'
       }, 300);
       $('#ide').animate({
-        paddingRight:'0',
-        paddingLeft:'0',
-        marginLeft:'0'
+        paddingRight: '0',
+        paddingLeft: '0',
+        marginLeft: '0'
       }, 400);
       $('#ide').animate({
         marginTop: '0px'
@@ -106,9 +108,9 @@ $(document).ready(function() {
         width: '96px'
       }, 300);
       $('#order').animate({
-        paddingRight:'0',
-        paddingLeft:'0',
-        marginLeft:'0'
+        paddingRight: '0',
+        paddingLeft: '0',
+        marginLeft: '0'
       }, 400);
       $('#order').animate({
         marginTop: '0px'
@@ -119,9 +121,9 @@ $(document).ready(function() {
         width: '96px'
       }, 300);
       $('#about').animate({
-        paddingRight:'0',
-        paddingLeft:'0',
-        marginLeft:'0'
+        paddingRight: '0',
+        paddingLeft: '0',
+        marginLeft: '0'
       }, 400);
       $('#about').animate({
         marginTop: '0px'
@@ -132,10 +134,11 @@ $(document).ready(function() {
         width: '96px'
       }, 300);
       $('#contact').animate({
-        paddingRight:'0',
-        paddingLeft:'0',
-        marginLeft:'0'
+        paddingRight: '0',
+        paddingLeft: '0',
+        marginLeft: '0'
       }, 400);
+
       $('#contact').animate({
         marginTop: '0px'
       }, 300, function() {
@@ -143,14 +146,52 @@ $(document).ready(function() {
           marginTop: '0px'
         }, 400);
 
+        $('.nav-button').css('display', 'none');
+
       });
 
-      number = 0;
+      menuNotActive = 0;
+        console.log(menuNotActive);
     }
   });
-  $('#twitter').hover(function(){
-    $('#twitter').css('width','60px');
-    $('#twitter').css('height','60px');
 
+
+  $('#logo').mouseover(function() {
+    $('#logo').animate({
+      width: '120px',
+      height: '120px',
+      marginLeft: '-10'
+
+    }, 200);
   });
+
+  $('#logo').mouseleave(function() {
+    if (menuNotActive==0 ) {
+      $('#logo').animate({
+        width: '96px',
+        height: '96px',
+        marginLeft: '0'
+
+      }, 200);
+
+    }
+  });
+  /*$(document).on('scroll', function(){
+
+    if($(document).scrollTop() > 15 && $('#logo').width()!=120 ){
+      $('#logo').animate({
+        width:'70px',
+        height:'70px',
+      },100);
+    }else{
+      if($(document).scrollTop() < 15){
+        $('#logo').animate({
+          width:'96px',
+          height:'96px',
+        },100);
+      }
+    }
+  });*/
+
+
 });
